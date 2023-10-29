866748727f5eb64dac90e0d207eff13a19f96e3f
jatin-chandela <147822681+jatin-chandela@users.noreply.github.com>
2023-10-30 03:26:17 +0530

    Create Expt43_preorderTraversal.cpp

---
+++ Expt43_preorderTraversal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void preorderTraversal(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    cout << root->val << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    preorderTraversal(root);
+
+    return 0;
+}
