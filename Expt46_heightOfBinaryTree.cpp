a5c3b02cecca4768eaa83b00e852d63eab877408
jatin-chandela <147822681+jatin-chandela@users.noreply.github.com>
2023-10-30 03:27:59 +0530

    Create Expt46_heightOfBinaryTree.cpp

---
+++ Expt46_heightOfBinaryTree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int height(TreeNode* root) {
+    if (root == nullptr) {
+        return 0;
+    }
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return 1 + max(leftHeight, rightHeight);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    int treeHeight = height(root);
+    cout << "Height of the binary tree: " << treeHeight << endl;
+
+    return 0;
+}
