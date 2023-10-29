8dbb02416d67237b67084600aee939180eb60ea6
jatin-chandela <147822681+jatin-chandela@users.noreply.github.com>
2023-10-30 03:31:08 +0530

    Create Expt53_insertionBST.cpp

---
+++ Expt53_insertionBST.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+};
+
+// Function to insert a new node into the BST
+TreeNode* insert(TreeNode* root, int value) {
+    if (root == nullptr) {
+        return new TreeNode(value);
+    }
+
+    if (value < root->val) {
+        root->left = insert(root->left, value);
+    } else if (value > root->val) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+// Inorder traversal to verify the BST
+void inorderTraversal(TreeNode* root) {
+    if (root == nullptr) return;
+
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+    TreeNode* root = nullptr;
+
+    // Insert nodes into the BST
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    cout << "Inorder traversal of the BST: ";
+    inorderTraversal(root);
+
+    return 0;
+}
